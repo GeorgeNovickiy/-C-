e1f6687b68a770bb59732d43a9a2e5c57308a5f8
George <madeinerussia@yandex.ru>
2022-11-14 15:53:14 +0500

    Add Task 00

---
+++ Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b) 
+{
+    Console.WriteLine($"Число {a} является квадратом {b}");
+}
+else 
+{
+    Console.WriteLine($"Число {a} не является квадратом {b}");
+}
