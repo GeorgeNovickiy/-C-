8efbd0f251cc8681b41a7bd259912f1067a20026
George <madeinerussia@yandex.ru>
2022-11-23 15:41:17 +0500

    Make changes to Task 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -5,13 +5,28 @@
 // 7 -> да
 // 1 -> нет
 
+// Решение через метод типа void 
+
+// Console.WriteLine("Введите номер дня недели");
+// int day = Convert.ToInt32(Console.ReadLine());
+// Weekend(day);
+
+// void Weekend(int numberDay)
+// {
+//     if ((numberDay >= 1) && (numberDay <= 5)) Console.WriteLine("Этот день не является выходным.");
+//     else if ((numberDay >= 6) && (numberDay <= 7)) Console.WriteLine("Этот день является выходным");
+//     else Console.WriteLine("Такого дня недели нет");
+// }
+
+
 Console.WriteLine("Введите номер дня недели");
 int day = Convert.ToInt32(Console.ReadLine());
-Weekend(day);
 
-void Weekend(int numberDay)
+if (Weekend(day)) Console.WriteLine("Этот день является выходным");
+else if ((day >= 1) && (day <= 5)) Console.WriteLine("Этот день не является выходным.");
+else Console.WriteLine("Такого дня недели нет");
+
+bool Weekend(int numberDay)
 {
-    if ((numberDay >= 1) && (numberDay <= 5)) Console.WriteLine("Этот день не является выходным.");
-    else if ((numberDay >= 6) && (numberDay <= 7)) Console.WriteLine("Этот день является выходным");
-    else Console.WriteLine("Такого дня недели нет");
+    return (numberDay >= 6) && (numberDay <= 7);
 }
