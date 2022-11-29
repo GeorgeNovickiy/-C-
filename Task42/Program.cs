a865e1c91004aa027f9017e38f5b03549a8cfb2b
George <madeinerussia@yandex.ru>
2022-11-29 13:31:24 +0500

    Make changes Task 42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -3,20 +3,65 @@
 // 3 -> 11
 // 2 -> 10
 
+// Решение через строковый тип данных 
 
-Console.WriteLine("Введите десятичное число:");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите десятичное число:");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-string ConvertToBin(int num)
+// string ConvertToBin(int num)
+// {
+//     string str = string.Empty;
+//     while (num > 0)
+//     {
+//         str = num % 2 + str;
+//         num = num / 2;
+//     }
+//     return str;
+// }
+
+// string newNumberBin = ConvertToBin(number);
+// Console.WriteLine($"Преобразованное двоичное число равно {newNumberBin}");
+
+
+/////////////////////////////////////////////////////////////////
+
+// Решение через массив int
+
+void PrintArray(int[] arr, string beginStr, string separatorElems, string endstr)
 {
-    string str = string.Empty;
-    while (num > 0)
+    Console.Write(beginStr);
+    for (int i = 0; i < arr.Length; i++)
     {
-        str = num % 2 + str;
-        num = num / 2;
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}{separatorElems}");
+        else Console.Write($"{arr[i]}");
     }
-    return str;
+    Console.Write(endstr);
 }
 
-string newNumberBin = ConvertToBin(number);
-Console.WriteLine($"Преобразованное двоичное число равно {newNumberBin}");
+
+int[] Binary(int numb)
+{
+    int numb1 = numb;
+    int digits = 0;
+    while (numb1 > 0)
+    {
+        numb1 /= 2;
+        digits += 1;
+
+    }
+    int[] bin = new int[digits];
+    for (int i = 0; i < bin.Length; i++)
+    {
+        bin[bin.Length-i-1] = numb % 2;
+        numb /= 2;
+    }
+    return bin;
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] binary = Binary(number);
+// Console.WriteLine(Binary(number));
+PrintArray(binary, "", "", "");
