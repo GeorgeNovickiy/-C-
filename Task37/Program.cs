17eca0e674f48513c2cc931db8b0a0a1392cf64d
George <madeinerussia@yandex.ru>
2022-11-29 12:35:12 +0500

    Add Task39

---
+++ Task37/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+
+
+int[] CreateArrayRndInt(int size, int min, int max) // Метод создания массива
+{
+    int[] arr = new int[size]; // по умолчанию создается массив размером size, заполненный нулями.
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreateArrayRndInt(4, 1, 100);
+PrintArray(array);
+
+int[] MultArray(int[] arr)
+{
+    int[] arr1 = arr.Length % 2 == 0 ? new int[arr.Length / 2] : new int[arr.Length / 2 + 1];
+    if (arr.Length % 2 == 0)
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        arr1[i] = arr[i] * arr[arr.Length-i-1];
+    }
+    else 
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        arr1[i] = arr[i] * arr[arr.Length-i-1] + arr[arr.Length/2+1];
+    }
+    return arr1;
+}
+
+int[] array2 = MultArray(array);
+PrintArray(array2);
---
+++ Task39/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] CreateArrayRndInt(int size, int min, int max) // Метод создания массива
+{
+    int[] arr = new int[size]; // по умолчанию создается массив размером size, заполненный нулями.
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+//Array.Reverse(array); - встроенный метод по переворачиванию массива
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+int[] arr = CreateArrayRndInt(5,1,9);
+PrintArray(arr);
+ReverseArray(arr);
+PrintArray(arr);
