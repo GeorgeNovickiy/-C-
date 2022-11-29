c07534811562312cfdbfc29ec5d60cd4b418bd5f
George <madeinerussia@yandex.ru>
2022-11-29 13:13:49 +0500

    Add Task 40, 42

---
+++ Task40/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и 
+// проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Введите длину первой стороны треугольника:");
+int firstLine = Convert.ToInt32(Console.ReadLine());        
+Console.WriteLine("Введите длину второй стороны треугольника:");
+int secondLine = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine("Введите длину третей стороны треугольника:");
+int thirdLine = Convert.ToInt32(Console.ReadLine());  
+
+bool Triangle(int a, int b, int c)
+{
+    if ((a+b) > c && (a+c) > b && (c+b) > a) return true;
+    else return false;
+}
+
+bool tri = Triangle(firstLine, secondLine, thirdLine);
+Console.WriteLine(tri ? "да" : "нет");
---
+++ Task42/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.WriteLine("Введите десятичное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string ConvertToBin(int num)
+{
+    string str = string.Empty;
+    while (num > 0)
+    {
+        str = num % 2 + str;
+        num = num / 2;
+    }
+    return str;
+}
+
+string newNumberBin = ConvertToBin(number);
+Console.WriteLine($"Преобразованное двоичное число равно {newNumberBin}");
