45c8d5507eade118897bceaaa747f2e5e3d2648c
George <madeinerussia@yandex.ru>
2022-12-02 14:02:56 +0500

    Make changes Task 51

--- Task51/Program.cs
+++ Task51/Program.cs
@@ -3,9 +3,13 @@
 // (0,0); (1;1) и т.д.
 
 
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // Метод создания массива
+// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-    int[,] matrix = new int[rows, columns]; // по умолчанию создается массив размером size, заполненный нулями.
+    int[,] matrix = new int[rows, columns];
     Random rnd = new Random();
 
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -13,41 +17,54 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // Метод
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             matrix[i, j] = rnd.Next(min, max + 1);
+            // TODO: вывести индексы массива.
+            // matrix[i, j] = i * 10 + j;
         }
-
     }
     return matrix;
 }
 
-void PrintMatrix(int[,] matrix)
+void PrintMatrix(int[,] matrix, string beginRow, string separatorElems, string endRow)
 {
-
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        Console.Write("|");
+        Console.Write(beginRow);
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],4}{separatorElems}");
             else Console.Write($"{matrix[i, j],4}");
         }
-        Console.WriteLine("|");
+        Console.WriteLine(endRow);
     }
 }
+// Это более затратный способ решения в плане компьютеной памяти
 
-int SumElemDiag(int[,] matrix)
+// int SumDiagonalMatrix(int[,] matrix)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i == j) sum += matrix[i, j];
+//         }
+//     }
+//     return sum;
+// }
+
+int SumDiagonalMatrix(int[,] matrix)
 {
-    int sumElemDiag = 0;
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (i == j) sumElemDiag = sumElemDiag + matrix[i, j];
-        }
-    }
-    return sumElemDiag;
+    int sum = 0;
+    int minIndex = matrix.GetLength(0);
+    if (matrix.GetLength(1) < matrix.GetLength(0)) minIndex = matrix.GetLength(1);
+    for (int i = 0; i < minIndex; i++)
+        sum += matrix[i, i];
+    return sum;
 }
 
-int[,] array = CreateMatrixRndInt(3, 4, 1, 9);
-PrintMatrix(array);
-int result = SumElemDiag(array);
-Console.WriteLine($"Сумма элементов находящихся на главной диагонали матрицы равна {result}");
+int[,] array = CreateMatrixRndInt(4, 3, 1, 9);
+PrintMatrix(array, "", "", "");
+Console.WriteLine();
+int sumDiagonalMatrix = SumDiagonalMatrix(array);
+Console.Write($"Сумма элементов, находящихся на главной диагонали = {sumDiagonalMatrix}");
