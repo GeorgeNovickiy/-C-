0a0bb9a8767bd5d67d0099f8c32a450249990c59
George <madeinerussia@yandex.ru>
2022-12-04 21:17:29 +0500

    Add Task 36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,7 +1,8 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
 
-// [345, 897, 568, 234] -> 2
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 
 int[] CreateArrayRndInt(int size, int min, int max) // Метод создания массива
 {
@@ -26,19 +27,17 @@ void PrintArray(int[] array)
     Console.WriteLine("]");
 }
 
-
-int NumEvenNumbers(int[] array)
+int SumElemUnevenPos(int[] array)
 {
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
+    int sum = 0;
+    for (int i = 1; i < array.Length; i=i+2)
     {
-        if (array[i] % 2 == 0) count++;
+        sum = sum + array[i];
     }
-    return count;
+    return sum;
 }
-
 
 int[] array = CreateArrayRndInt(6, 100, 1000);
 PrintArray(array);
-int numEvenNum = NumEvenNumbers(array);
-Console.WriteLine($"Количество четных чисел в массиве = {numEvenNum}");
+int sumElemUnevenPos=SumElemUnevenPos(array);
+Console.WriteLine($"Сумма элементов стоящих на нечетной позиции равна {sumElemUnevenPos}");
