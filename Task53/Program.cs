c0456c6f343e84b88962804a2a97c41946800a28
George <madeinerussia@yandex.ru>
2022-12-09 05:59:14 +0500

    Add Task 53

---
+++ Task53/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 53: Задайте двумерный массив. 
+// Напишите программу,которая поменяет местами первую и последнюю строку массива.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+            // TODO: вывести индексы массива.
+            // matrix[i, j] = i * 10 + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string beginRow, string separatorElems, string endRow)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write(beginRow);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],4}{separatorElems}");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine(endRow);
+    }
+}
+
+int[,] ChangeRows(int[,] matrix)
+{
+    int[] arr = new int[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        arr[j] = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0)-1, j];
+        matrix[matrix.GetLength(0)-1, j] = arr[j];
+    }
+    return matrix;
+}
+
+
+int[,] array = CreateMatrixRndInt(4, 3, 1, 9);
+PrintMatrix(array, "", "", "");
+Console.WriteLine();
+int[,] result = ChangeRows(array);
+PrintMatrix(result, "", "", "");
