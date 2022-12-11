b58eb1d9864022e78f6efa679c6d0a132463dc48
George <madeinerussia@yandex.ru>
2022-12-11 19:39:23 +0500

    Add Task 57

---
+++ Task57/Program.cs
@@ -0,0 +1,91 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+            // TODO: вывести индексы массива.
+            // matrix[i, j] = i * 10 + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string beginRow, string separatorElems, string endRow)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write(beginRow);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],4}{separatorElems}");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine(endRow);
+    }
+}
+
+int[] MatrixToArray(int[,] matrix)
+{
+    int[] arr = new int[matrix.Length];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            arr[k] = matrix[i, j];
+            k++;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void CountValue(int[] arr)
+{
+    int count = 1;
+    int num = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] == num) count++;
+        else
+        {
+            Console.WriteLine($"{num} встречается {count} раз(а)");
+            count = 1;
+            num = arr[i];
+        }
+    }
+    Console.WriteLine($"{num} встречается {count} раз(а)");
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D, "", "", "");
+Console.WriteLine();
+
+int[] array = MatrixToArray(array2D);
+PrintArray(array);
+Console.WriteLine();
+
+Array.Sort(array);
+PrintArray(array);
+Console.WriteLine();
+
+CountValue(array);
